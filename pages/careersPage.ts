53bd16a149725d0ac93d2bd541b144623430bcce
Eloi Taniguti <eloi.taniguti@nearform.com>
2022-08-16 16:10:43 -0300

    Add careers page tests

---
+++ pages/careersPage.ts
@@ -0,0 +1,66 @@
+import { expect, FrameLocator, Locator, Page } from "@playwright/test";
+import * as results from "../data/results/appResults.json";
+
+
+export class CareersPage {
+    // Types definition
+    readonly page: Page;
+    readonly faqAnswers: Locator;
+    readonly faqQuestions: Locator;
+    readonly filterLabel: Locator;
+    readonly filterOptions: Locator;
+    readonly iframe: FrameLocator;
+    readonly nearformersSayAuthors: Locator;
+    readonly nearformersSayParagraphs: Locator;
+    readonly nearformersSayTitles: Locator;
+    readonly subTitles: Locator;
+    readonly title: Locator;
+    readonly values: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.iframe = page.frameLocator('#grnhse_iframe');
+        this.faqAnswers = page.locator('#career-toggles >> p');
+        this.faqQuestions = page.locator('#career-toggles >> h3');
+        this.nearformersSayAuthors = page.locator('#content >> .fusion-content-boxes >> figcaption');
+        this.nearformersSayParagraphs = page.locator('#content >> .fusion-content-boxes >> p');
+        this.nearformersSayTitles = page.locator('#content >> .fusion-content-boxes >> h3');
+        this.subTitles = page.locator('#content >> h2[class ^= "title-heading"]');
+        this.title = page.locator('#content >> h1[class ^= "title-heading"]');
+        this.values = page.locator('#content >> .fusion-text > p');
+
+        this.filterLabel = this.iframe.locator('.filter-label');
+        this.filterOptions = this.iframe.locator('#departments-select > option');
+    };
+
+    async assertPageTitle() {
+        await expect(this.page).toHaveTitle(results.careersTitle);
+    };
+
+    async assertTitles() {
+        await expect(this.title).toHaveText(results.pages.careers.title);         
+        await expect(this.subTitles).toHaveText(results.pages.careers.subTitles);         
+    };
+
+    async assertNFValues() {
+        await expect(this.values).toHaveText(results.pages.careers.values);      
+    };
+
+    async assertNearFormersSay() {
+        await expect(this.nearformersSayTitles).toHaveText(results.pages.careers.nfSayTitles);
+        await expect(this.nearformersSayParagraphs).toHaveText(results.pages.careers.nfSayParagraphs);
+        await expect(this.nearformersSayAuthors).toHaveText(results.pages.careers.nfSayAuthors);
+    };
+
+    async assertFAQ() {
+        await expect(this.faqQuestions).toHaveText(results.pages.careers.faqQuestions);
+        await expect(this.faqAnswers).toHaveText(results.pages.careers.faqAnswers);
+    };
+
+    async assertJobsFilter() {
+        await expect(this.filterLabel).toHaveText(results.pages.careers.filterLabel);
+        await expect(this.filterOptions).toHaveText(results.pages.careers.filterOptions);
+    };
+
+
+};
---
+++ tests/careersPage.spec.ts
@@ -0,0 +1,36 @@
+import { test } from '@playwright/test';
+import { CareersPage } from '../pages/careersPage';
+
+import { Utils } from '../utils/utils';
+
+test.describe('Careers Page tests', () => {
+    test.beforeEach(async ({ page }) => {
+      const utils = new Utils(page);
+      const careers = new CareersPage(page); 
+      await utils.visit('/careers/');
+      // Expect the page has the correct title.
+      await careers.assertPageTitle();
+    });
+  
+    test.describe.skip('Accessibility test', () => {
+      test('verify home page passes A11y', async ({ page }) => {
+        const utils = new Utils(page);
+        utils.pageAccessibilityCheck(page);
+      });
+    });
+
+    test.describe('Page Content tests', () => {
+        test('verify Main content', async ({ page }) => {
+            const careers = new CareersPage(page); 
+
+            await careers.assertTitles();
+            await careers.assertNFValues();
+
+            await careers.assertNearFormersSay();
+            await careers.assertFAQ();
+
+            await careers.assertJobsFilter();
+        });
+      
+    });
+});  
